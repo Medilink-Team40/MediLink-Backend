1dbb690902d60003aa24a3dbd2e957697f0ce813
ndiezcoppel <ndiez@coppel.com.ar>
2025-10-27 13:49:20 -0300

    feat: patient service

--- src/patient/controllers/patient/patient.controller.ts
+++ src/patient/controllers/patient/patient.controller.ts
@@ -3,15 +3,19 @@ import { KeyCloakService } from '../../../keycloak/services/create/create.servic
 import { KeycloakCreateDto } from '../../../keycloak/dto/KeycloakDto';
 import { PatientRegisterDto } from '../../dto/PatientRegisterDto';
 import { RolesTypes } from '../../../auth/auth.types';
+import { PatientService } from '../../services/patient/patient.service';
 
 @Controller('patient')
 export class PatientController {
-  constructor(private readonly keycloak: KeyCloakService) {}
+  constructor(
+    private readonly keycloak: KeyCloakService,
+    private readonly service: PatientService,
+  ) {}
 
   public async create(@Body() patient: PatientRegisterDto) {
     const keycloakid = await this.keycloak.createUserAndAssignRole(new KeycloakCreateDto(patient), RolesTypes.PATIENT);
 
-    const user = await this.service.create(practitioner, keycloakid);
+    const user = await this.service.create(patient, keycloakid);
     return user;
   }
 }
--- src/patient/services/patient/patient.service.ts
+++ src/patient/services/patient/patient.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PatientRegisterDto } from '../../dto/PatientRegisterDto';
+import { Patient } from '../../entities/patient.entity';
+import { RolesTypes } from '../../../auth/auth.types';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PatientService {}
+export class PatientService {
+  constructor(
+    @InjectRepository(Patient)
+    private readonly repository: Repository<Patient>,
+  ) {}
+
+  public async create(dto: PatientRegisterDto, id: string) {
+    const { repeatpassword, password, ...requiredData } = dto;
+    const entity = {
+      ...requiredData,
+      keycloakId: id,
+      active: false,
+      role: RolesTypes.PATIENT,
+    } as unknown as Patient;
+
+    const newPractitioner = this.repository.create(entity);
+    return await this.repository.save(newPractitioner);
+  }
+}
