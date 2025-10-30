958f91a8cf1d0d73b08efea215f4871946dade78
OrlandoCV <orlandocv0107@gmail.com>
2025-10-30 18:52:22 -0300

    fix : import module in practitioner.module

--- src/practitioner/practitioner.module.ts
+++ src/practitioner/practitioner.module.ts
@@ -11,18 +11,18 @@ import { PractitionerUpdaterFactory } from './factory/updater.factory';
 import { QualificationController } from './controllers/qualification/qualification.controller';
 import { QualificationService } from './service/qualification/qualification.service';
 import { QualificationCode } from './entities/qualification-codes.entity';
-import { CalendarService } from '../calendar/calendar.service';
-import { CalendarEntity } from '../calendar/entity/calendar.entity';
+import { CalendarModule } from '../calendar/calendar.module';
 
 @Module({
   imports: [
     HttpModule,
     PassportModule,
     KeycloakModule,
-    TypeOrmModule.forFeature([Practitioner, PractitionerIdentifier, PractitionerQualification, PractitionerTelecom, QualificationCode, CalendarEntity]),
+    CalendarModule,
+    TypeOrmModule.forFeature([Practitioner, PractitionerIdentifier, PractitionerQualification, PractitionerTelecom, QualificationCode]),
   ],
   controllers: [PractitionerController, QualificationController],
-  providers: [KeyCloakService, PractitionerService, PractitionerUpdaterFactory, QualificationService, CalendarService],
+  providers: [KeyCloakService, PractitionerService, PractitionerUpdaterFactory, QualificationService],
   exports: [PractitionerService],
 })
 export class PractitionerModule { }
